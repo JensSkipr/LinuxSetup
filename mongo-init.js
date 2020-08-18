d2e55106b7c5cd5db624f28566fc3397f0428649
Jens Skipr <62247524+JensSkipr@users.noreply.github.com>
2020-08-18 11:21:55 +0200

    Simplify setup

--- mongo-init.js
+++ mongo-init.js
@@ -1,99 +1,3 @@
-// =======================================
-// =             "REAL" USER             =
-// =======================================
-db['api-mobile-authentication_providers_mapping'].insert({
-    _id: 'J7aAUEXZavUunkDBrjQuPCqBi8M2',
-    user_id: 'J7aAUEXZavUunkDBrjQuPCqBi8M2',
-    provider_authentications: [
-        {
-            user_reference: 'J7aAUEXZavUunkDBrjQuPCqBi8M2',
-            provider: 'firebase'
-        }
-    ]
-})
-
-db['srv-user-users'].insert({
-    "_id": "J7aAUEXZavUunkDBrjQuPCqBi8M2",
-    "first_name": "RealFirst",
-    "last_name": "LocalLast",
-    "email": "cd9xk@email.com",
-    "phone_number": "+32475000000",
-    "accepted_terms_and_conditions": true,
-    "accepted_terms_and_conditions_version": "01/04/2019",
-    "date_of_birth": ISODate("1919-09-18T00:00:00.000Z"),
-    "gender": "Female",
-    "is_blocked": true,
-    "organisation_links": [
-        {
-            "organisation_id": "accenture",
-            "link_type": "confirm",
-            "credentials": [
-                {
-                    "provider": "poppy",
-                    "login": "apescheur@vulog.com",
-                    "password": "Cassy06"
-                },
-                {
-                    "provider": "delijn"
-                },
-                {
-                    "provider": "sncb"
-                }
-            ]
-        }
-    ],
-    "credentials": [
-        {
-            "provider": "sncb"
-        },
-        {
-            "provider": "mobib",
-            "provider_user_id": "6060575401800144639"
-        }
-    ],
-    "created_at": ISODate("2019-06-17T13:25:54.831Z"),
-    "updated_at": ISODate("2020-04-21T08:08:51.495Z"),
-    "payment_profiles": [
-        {
-            "payment_profile_id": "3cf74c3a-da36-4906-bb45-5bab5d6bf0cd",
-            "title": {
-                "fr": "Privé",
-                "nl": "Private",
-                "en": "Private"
-            },
-            "order": 1,
-            "profile_type": "wallet",
-            "url": "https://url",
-            "membership_reference": "b59a4841-50a4-4f2d-a598-0400554f5c9f",
-            "organisation_reference": "340f4d62-233b-420f-94ab-64bb741b27fe",
-            "expense_manager": "skipr_for_business",
-            "expense_user_reference": "0c0dd99c-10c0-48ac-9e4f-e3c96fc017fb",
-            "wallet_type": "private"
-        },
-        {
-            "payment_profile_id": "51f190fc-c0db-4be8-b401-56ee5ad7e58a",
-            "title": {
-                "fr": "Professionnel",
-                "nl": "Business",
-                "en": "Business"
-            },
-            "order": 2,
-            "profile_type": "wallet",
-            "url": "https://url",
-            "membership_reference": "b59a4841-50a4-4f2d-a598-0400554f5c9f",
-            "organisation_reference": "340f4d62-233b-420f-94ab-64bb741b27fe",
-            "expense_manager": "skipr_for_business",
-            "expense_user_reference": "0c0dd99c-10c0-48ac-9e4f-e3c96fc017fb",
-            "wallet_type": "pro"
-        }
-    ],
-    "last_login": ISODate("2020-04-21T08:08:51.288Z"),
-    "language": "en"
-})
-
-// =======================================
-// =             MOCKED USER             =
-// =======================================
 db['api-mobile-authentication_providers_mapping'].insert({
     _id: 'F4UOX2a6peWd9Th3KYFtkUpqKmQ2',
     user_id: 'F4UOX2a6peWd9Th3KYFtkUpqKmQ2',
@@ -107,10 +11,10 @@ db['api-mobile-authentication_providers_mapping'].insert({
 
 db['srv-user-users'].insert({
     "_id": "F4UOX2a6peWd9Th3KYFtkUpqKmQ2",
-    "first_name": "MockedFirst",
+    "first_name": "LocalFirst",
     "last_name": "LocalLast",
     "email": "cd9xk@email.com",
-    "phone_number": "+32475000009",
+    "phone_number": "+32475000000",
     "accepted_terms_and_conditions": true,
     "accepted_terms_and_conditions_version": "01/04/2019",
     "date_of_birth": ISODate("1919-09-18T00:00:00.000Z"),
