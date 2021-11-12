412ad3ff5710ffdcd2b5a3e363eb477b88e295a9
JensSkipr <62247524+JensSkipr@users.noreply.github.com>
2021-11-12 08:31:01 +0100

    go_test_all: Include integration tests

--- go_test_all.sh
+++ go_test_all.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find -mindepth 1 -maxdepth 1 -type d -exec sh -c "cd {}; pwd; go test ./...; cd - > /dev/null" \;
+find -mindepth 1 -maxdepth 1 -type d -exec sh -c "cd {}; pwd; go test --tags integration ./...; cd - > /dev/null" \;
